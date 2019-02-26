947d29f32401d813514859bd8012435f4e3efd26
GoatFeatherFarm <44263291+missybarringer@users.noreply.github.com>
2019-02-26 12:10:12 -0600

    refactored code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,123 +8,143 @@
 // end of game print "All done, here's how you did!" followed by correct, incorrect, & unanswered tallies
 // Start Over? click will start the game over again  (without reloading the page)
 
+$(document).ready(function() {
 
-var startScreen;
-var gameHTML;
-var counter = 15;
-var questionCounter = 0;
-var selectorAnswer;
-var theClock;
-var correct = 0;
-var incorrect = 0;
-var unanswered = 0;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unansweredQuestions = 0;
+var timeRemaining = 16;
+var intervalID;
 var indexQandA = 0;
-
-var questionArray = ["What is the fastest animal?", "When you are blue you turn?"];
-var answerArray = [["cheetah","turtle","giraffe", "elephant"],["red", "green", "blue", "yellow"]];
-var correctAnswers = ["A. cheetah", "C. blue"];
-
-var triviaGame = [{
+var answered = false;
+var correct;
+    
+    var start = $(".start").html("Start Game");
+    start.on("click", startGame);
+    
+var triviaQandA = [{
     question:"What is the fastest animal?",
     answer:["cheetah","turtle","giraffe","elephant"],
     correct:"0",
     image: ("assets/images/circle.png")
 }, { 
-    question:"When you are upset you turn?",
-    answer:["red","green","blue","yello"],
-    correct:"1",
+    question:"When you are blue you turn?",
+    answer:["red","green","blue","yellow"],
+    correct:"2",
     image: ("assets/images/dot.jpg")
 }];
 
-$(document).ready(function() {
-function initialScreen() {
-    startScreen = "<p class='text-center main-button-container'><a class='btn btn-warning btn-lg btn-block start-button' href='#' role='button'>Start Quizz</a></p>";
-    $(".content").html(startScreen);
+function startGame() {
+    $(".summary").hide();
+    $(".start").hide();
+    $(".timeRemaining").show();
+    $(".question").show();
+    $(".answers").show();
+    $(".start-over").hide();
+    correctAnswers = 0;
+    incorrectAnswers = 0;
+    unansweredQuestions = 0;
+    loadQandA();
 }
-initialScreen();
-  
-$(".start-button").on("click", function() {
-    generateHTML();
-    timerWrapper();
-});
 
-// $(".answer").on("click", function() {
-$("body").on("click", ".answer", function() {
-    selectedAnswer = $(this).text();
-    if(selectedAnswer === correctAnswers[questionCounter]) {
-        clearInterval(theClock);
-        generateWin();
-    } else {
-        clearInterval(theClock);
-        generateLoss();
+function loadQandA() {
+    answered = false;
+    timeRemaining = 16;
+    intervalID = setInterval(timer, 1000);
+    if (answered === false) {
+        timer();
     }
-});
-
-// $(".reset-button").on("click", function() {
-    $("body").on("click", ".reset-button", function() {
-    resetGame();
-});
-
-});
-
-function generateLossDueToTimeOut() {
-    unanswered++;
-    gameHTML = "<p class='text-center timer-p'>Time remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text center'>Times UP! The answer is: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='assets/images/circle.png'>";
-    $(".content").html(gameHTML);
-    setTimeout(wait, 1000);
-}
-function generateWin() {
-    correct++;
-    gameHTML = "<p class='text-center timer-p'>Time remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text center'>Righty roo! The answer is: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='assets/images/dot.jpg'>";
-    $(".content").html(gameHTML);
-    setTimeout(wait, 1000);
-}
-function generateLoss() {
-    incorrect++;
-    gameHTML = "<p class='text-center timer-p'>Time remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text center'>Nooo! The answer is: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='assets/images/circle.png'>";
-    $(".content").html(gameHTML);
-    setTimeout(wait, 1000);
-}
-function generateHTML() {
-    gameHTML = "<p class='text-center timer-p'>Time remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text center'>"+ questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'B. " + answerArray[questionCounter][1] + "</p><p class='answer'>C. " + answerArray[questionCounter][2] + "</p><p class='answer'>D. " + answerArray[questionCounter][3] + "</p>";
-    $(".content").html(gameHTML);
     
+    correct = triviaQandA[indexQandA].correct;
+    var question = triviaQandA[indexQandA].question;
+    $(".question").html(question);
+    for (var i = 0; i < 4; i++) {
+        var answer = triviaQandA[indexQandA].answer[i];
+        $(".answers").append("<h4 class = answersAll id =" + i + ">" + answer + "</h4>");
+    }
+
+    $("h4").click(function() {
+        var id = $(this).attr("id");
+        if (id === correct) {
+            answered = true;
+            $(".question").text("The answer is: " + triviaQandA[indexQandA].answer[correct]);
+            correctAnswer();
+        } else {
+            answered = true;
+            $(".question").text("You chose: " + triviaQandA[indexQandA].answer[id] + "    the correct answer is: " + triviaQandA[indexQandA].answer[correct]);
+            incorrectAnswer();
+        }
+        console.log(correct);
+    });
 }
-function wait() {
-    if (questionCounter < 1) {
-        questionCounter++;
-        generateHTML();
-        counter = 30;
-        timerWrapper();
+
+function timer() {
+    if (timeRemaining === 0) {
+        answered = true;
+        clearInterval(intervalID);
+        $(".question").text("The correct answer is: " + triviaQandA[indexQandA].answer[correct]);
+        unAnswered();
+    } else if (answered === true) {
+        clearInterval(intervalID);
     } else {
-        finalScreen();
-    }
-}
-function timerWrapper() {
-    theClock = setInterval(thirtySeconds, 1000);
-    function thirtySeconds() {
-        if (counter === 0) {
-            clearInterval(theClock);
-            generateLossDueToTimeOut();
-        }
-        if (counter > 0) {
-            counter--;
-        }
-        $(".timer").html(counter);
+        timeRemaining--;
+        $(".timeRemaining").text("You have " + timeRemaining);
     }
 }
 
-function finalScreen() {
-    gameHTML = "<p class='text-center timer-p'>Time remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Let's see how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correct + "</p>" + "<p>Wrong Answers: " + incorrect + "</p>" + "<p>Unanswered: " + unanswered + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-danger btn-lg btn-block reset-button' href='#' role='button'>Reset the quiz</a></p>";
-    $(".content").html(gameHTML);
+function correctAnswer() {
+    correctAnswers++;
+    $(".timeRemaining").text("You are spot on!!!");
+    reset();
 }
 
-function resetGame() {
-    questionCounter = 0;
-    correct = 0;
-    incorrect = 0;
-    unanswered = 0;
-    counter = 30;
-    generateHTML();
-    timerWrapper();
+function incorrectAnswer() {
+    incorrectAnswers++;
+    $(".timeRemaining").text("You are sooo wrong");
+    reset();
 }
+
+function unAnswered() {
+    unansweredQuestions++;
+    $(".timeRemaining").text("You didn't choose anything...");
+    reset();
+}
+
+function reset() {
+    $(".answersAll").remove();
+    $(".answers").append("<img class=answerImage width='150' height='150' src='" + triviaQandA[indexQandA].image + "'>");
+    indexQandA++;
+    if (indexQandA < triviaQandA.length) {
+        setTimeout(function () {
+            loadQandA();
+            $(".answerImage").remove();
+        }, 2000);
+    } else {
+        setTimeout(function() {
+            $(".summary").show();
+            $(".question").hide();
+            $(".timeRemaining").hide();
+            $(".answers").hide();
+            $(".start-over").show();
+            $(".answerImage").remove();
+            $(".summary").append("<h4 class = answersAll end>Correct answers: " + correctAnswers + "</h4>");
+            $(".summary").append("<h4 class = answersAll end>Wrong answers: " + incorrectAnswers + "</h4>");
+            $(".summary").append("<h4 class = answersAll end>Unanswered questions: " + unansweredQuestions + "</h4>");
+            correctAnswers = 0;
+            incorrectAnswers = 0;
+            unansweredQuestions = 0;
+            indexQandA = 0;
+            // setTimeout(function() {
+            //     location.reload();
+            // }, 5000);
+    
+            var start = $(".start-over").html("Start Game");
+            start.on("click", startGame);
+        }, 2000);
+    }
+};
+
+    
+});
+
+
