eff3501fba0977d99c0ddf214101774aca7547a9
GoatFeatherFarm <44263291+missybarringer@users.noreply.github.com>
2019-02-27 11:02:30 -0600

    updated pages with bug fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,141 +10,152 @@
 
 $(document).ready(function() {
 
-var correctAnswers = 0;
-var incorrectAnswers = 0;
-var unansweredQuestions = 0;
-var timeRemaining = 16;
-var intervalID;
-var indexQandA = 0;
-var answered = false;
-var correct;
-    
-    var start = $(".start").html("<p class='btn btn-warning btn-md btn-block'>Start Game</p>");
+    var correctAnswers = 0;
+    var incorrectAnswers = 0;
+    var unansweredQuestions = 0;
+    var timeRemaining = 16;
+    var intervalID;
+    var indexQandA = 0;
+    var answered = false;
+    var correct;
+        
+    var start = $(".start").html("Start Game");
     start.on("click", startGame);
+    $(".start-over").on("click", startGame);
+        
+    var triviaQandA = [{
+        question:"What is the fastest animal?",
+        answer:["cheetah","turtle","giraffe","elephant"],
+        correct:"0",
+        image: ("assets/images/circle.png")
+    }, { 
+        question:"When you are blue you turn?",
+        answer:["red","green","blue","yellow"],
+        correct:"1",
+        image: ("assets/images/dot.jpg")
+    }];
     
-var triviaQandA = [{
-    question:"In New York it is illegal to shoot rabbits from a what?",
-    answer:["Cab","Apartment window","Trolley","Horse"],
-    correct:"2",
-    image: ("assets/images/trolleyCar.jpg")
-}, { 
-    question:"In Vermont, a woman can't wear what without written permission from their husband?",
-    answer:["Shawl","Wig","False teeth","Hat"],
-    correct:"2",
-    image: ("assets/images/falseTeeth.jpg")
-}];
-
-function startGame() {
-    $(".summary").hide();
-    $(".start").hide();
-    $(".timeRemaining").show();
-    $(".question").show();
-    $(".answers").show();
-    $(".start-over").hide();
-    correctAnswers = 0;
-    incorrectAnswers = 0;
-    unansweredQuestions = 0;
-    loadQandA();
-}
-
-function loadQandA() {
-    answered = false;
-    timeRemaining = 16;
-    intervalID = setInterval(timer, 1000);
-    if (answered === false) {
+    function startGame() {
+        $(".summary").hide();
+        $(".start").hide();
+        $(".timeRemaining").show();
+        $(".question").show();
+        $(".answers").show();
+        $(".start-over").hide();
+        correctAnswers = 0;
+        incorrectAnswers = 0;
+        unansweredQuestions = 0;
+        loadQandA();
+    }
+    
+    function loadQandA() {
+        answer = [];
+        answered = false;
+        timeRemaining = 16;
         timer();
-    }
-    
-    correct = triviaQandA[indexQandA].correct;
-    var question = triviaQandA[indexQandA].question;
-    $(".question").html(question);
-    for (var i = 0; i < 4; i++) {
-        var answer = triviaQandA[indexQandA].answer[i];
-        $(".answers").append("<h4 class = answersAll id =" + i + ">" + answer + "</h4>");
-    }
-
-    $("h4").click(function() {
-        var id = $(this).attr("id");
-        if (id === correct) {
-            answered = true;
-            $(".question").text("The answer is: " + triviaQandA[indexQandA].answer[correct]);
-            correctAnswer();
-        } else {
-            answered = true;
-            $(".question").text("You chose: " + triviaQandA[indexQandA].answer[id] + "    the correct answer is: " + triviaQandA[indexQandA].answer[correct]);
-            incorrectAnswer();
+        intervalID = setInterval(timer, 1000);
+        /*if (answered === false) {
+            timer();
+        }*/
+        
+        correct = triviaQandA[indexQandA].correct;
+        var question = triviaQandA[indexQandA].question;
+        $(".question").html(question);
+        for (var i = 0; i < 4; i++) {
+            var answer = triviaQandA[indexQandA].answer[i];
+            $(".answers").append("<h4 class = answersAll id =" + i + ">" + answer + "</h4>");
         }
-        console.log(correct);
+    
+        $("h4").click(function() {
+            var id = $(this).attr("id");
+            if (id === correct) {
+                answered = true;
+                $(".question").text("The answer is: " + triviaQandA[indexQandA].answer[correct]);
+                correctAnswer();
+            } else {
+                answered = true;
+                $(".question").text("You chose: " + triviaQandA[indexQandA].answer[id] + "    the correct answer is: " + triviaQandA[indexQandA].answer[correct]);
+                incorrectAnswer();
+            }
+            console.log(correct);
+        });
+    }
+    
+    function timer() {
+        if (timeRemaining === 0) {
+            answered = true;
+            //clearInterval(intervalID);
+            $(".question").text("The correct answer is: " + triviaQandA[indexQandA].answer[correct]);
+            unAnswered();
+        } else if (answered === true) {
+            //clearInterval(intervalID);
+        } else {
+            timeRemaining--;
+            $(".timeRemaining").text("You have " + timeRemaining);
+        }
+    }
+    
+    function correctAnswer() {
+        correctAnswers++;
+        $(".timeRemaining").text("You are spot on!!!").css({
+            "color": "#3d414f"
+        });
+        reset();
+    }
+    
+    function incorrectAnswer() {
+        incorrectAnswers++;
+        $(".timeRemaining").text("You are sooo wrong").css({
+            "color": "#3d414f"
+        });
+        reset();
+    }
+    
+    function unAnswered() {
+        unansweredQuestions++;
+        $(".timeRemaining").text("You didn't choose anything...").css({
+            "color": "#3d414f"
+        });
+        reset();
+    }
+    
+    function reset() {
+        clearInterval(intervalID);
+        $(".answersAll").remove();
+        $(".answers").append("<img class=answerImage width='150' height='150' src='" + triviaQandA[indexQandA].image + "'>");
+        indexQandA++;
+        if (indexQandA < triviaQandA.length) {
+            setTimeout(function () {
+                loadQandA();
+                $(".answerImage").remove();
+            }, 2000);
+        } else {
+            setTimeout(function() {
+                $(".summary").show();
+                $(".question").hide();
+                $(".timeRemaining").hide();
+                $(".answers").hide();
+                $(".start-over").show();
+                $(".answerImage").remove();
+                $(".summary").append("<h4 class = answersAll end>Correct answers: " + correctAnswers + "</h4>");
+                $(".summary").append("<h4 class = answersAll end>Wrong answers: " + incorrectAnswers + "</h4>");
+                $(".summary").append("<h4 class = answersAll end>Unanswered questions: " + unansweredQuestions + "</h4>");
+                correctAnswers = 0;
+                incorrectAnswers = 0;
+                unansweredQuestions = 0;
+                indexQandA=0;
+                // setTimeout(function() {
+                //     location.reload();
+                // }, 5000);
+        
+                $(".start-over").html("Start Game");
+                start.on("click", startGame);
+            }, 2000);
+        }
+    };
+    
+        
     });
-}
-
-function timer() {
-    if (timeRemaining === 0) {
-        answered = true;
-        clearInterval(intervalID);
-        $(".question").text("The correct answer is: " + triviaQandA[indexQandA].answer[correct]);
-        unAnswered();
-    } else if (answered === true) {
-        clearInterval(intervalID);
-    } else {
-        timeRemaining--;
-        $(".timeRemaining").text("You have " + timeRemaining);
-    }
-}
-
-function correctAnswer() {
-    correctAnswers++;
-    $(".timeRemaining").text("You are spot on!!!");
-    reset();
-}
-
-function incorrectAnswer() {
-    incorrectAnswers++;
-    $(".timeRemaining").text("You are sooo wrong");
-    reset();
-}
-
-function unAnswered() {
-    unansweredQuestions++;
-    $(".timeRemaining").text("You didn't choose anything...");
-    reset();
-}
-
-function reset() {
-    $(".answersAll").remove();
-    $(".answers").append("<img class=answerImage width='150' height='150' src='" + triviaQandA[indexQandA].image + "'>");
-    indexQandA++;
-    if (indexQandA < triviaQandA.length) {
-        setTimeout(function () {
-            loadQandA();
-            $(".answerImage").remove();
-        }, 2000);
-    } else {
-        setTimeout(function() {
-            $(".summary").show();
-            $(".question").hide();
-            $(".timeRemaining").hide();
-            $(".answers").hide();
-            $(".start-over").show();
-            $(".answerImage").remove();
-            $(".summary").append("<h4 class = answersAll end>Correct answers: " + correctAnswers + "</h4>");
-            $(".summary").append("<h4 class = answersAll end>Wrong answers: " + incorrectAnswers + "</h4>");
-            $(".summary").append("<h4 class = answersAll end>Unanswered questions: " + unansweredQuestions + "</h4>");
-            correctAnswers = 0;
-            incorrectAnswers = 0;
-            unansweredQuestions = 0;
-            indexQandA = 0;
-            // setTimeout(function() {
-            //     location.reload();
-            // }, 5000);
     
-            var start = $(".start-over").html("<p class='btn btn-danger btn-md btn-block'>Restart Game?</p>");
-            start.on("click", startGame);
-        }, 2000);
-    }
-};
-
-    
-});
 
 
