7629aebf7828c8a37f3b78f581f3fb400b676183
GoatFeatherFarm <44263291+missybarringer@users.noreply.github.com>
2019-02-27 16:34:30 -0600

    final changes & styling

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # TriviaGame
+
+## Contributors
+@missybarringer
+____________________________________
+## Technology
+* CSS3, HTML5, Javascript, Bootstrap, jQuery
+* [GitHub Repository Link](https://github.com/missybarringer/unit-4-game.git)
+____________________________________
+## About - Vandy Bootcamp assignment with the following instructions...
+### Overview
+
+In this assignment, You'll create a Trivia game using JavaScript for the logic and jQuery to manipulate HTML. Be sure to layout this app with valid HTML and stylish CSS.
+
+### Option Two: Advanced Assignment (Timed Questions)
+
+**[Click Here to Watch the demo](https://youtu.be/xhmmiRmxQ8Q)**.
+
+* You'll create a trivia game that shows only one question until the player answers it or their time runs out.
+
+* If the player selects the correct answer, show a screen congratulating them for choosing the right option. After a few seconds, display the next question -- do this without user input.
+
+* The scenario is similar for wrong answers and time-outs.
+
+  * If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
+  * If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
+
+* On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
+
+### Add To Your Portfolio
+
+After completing the homework please add the piece to your portfolio. Make sure to add a link to your updated portfolio in the comments section of your homework so the TAs can easily ensure you completed this step when they are grading the assignment. To receive an 'A' on any assignment, you must link to it from your portfolio.
+
+#### Contributors
+* Background picture came from dmitrij-paskevic-44124-unsplash
+* Questions came from: [Trivia Country](https://www.triviacountry.com/336-legal-trivia-questions.htm)
+* and [BuzzNigeria](https://buzznigeria.com/fun-trivia-questions-answers/)
+____________________________________
+## License
+*This product is licensed under the MIT License (MIT).
+____________________________________
+## Contributing Guidelines
+All contributions and suggestions are welcome!
+For direct contributions, please fork the repository and file a pull request.
+____________________________________
+## Contact
+* [Website published here](https://missybarringer.github.io/unit-4-game/)
+* e-mail: barringer.margaret@gmail.com
+* Twitter: @goatfeatherfarm
+* Facebook: @goatfeatherfarm
+* Instagram: @goatfeatherfarm
+* Added to [Personal Portfolio webpage](https://missybarringer.github.io/)
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,15 @@
 // create a splash page with a start button, once clicked go to first question
 // display timer countdown number, question, and all possible answers
 // if answer chosen then decide if it was correct or not & increment appropriate right/wrong counter
-// if wrong display "Nope" and correct answer & pic
-// if right display "Yuppers" & pic
+// if wrong display "You are wrong" and correct answer & pic
+// if right display "You are spot on" & pic
 // if answer not chosen then display "You zoned out" & correct answer & pic
 // After a few seconds, display the next question without user input
 // end of game print "All done, here's how you did!" followed by correct, incorrect, & unanswered tallies
 // Start Over? click will start the game over again  (without reloading the page)
 
 $(document).ready(function() {
-
+    // global variable declarations
     var correctAnswers = 0;
     var incorrectAnswers = 0;
     var unansweredQuestions = 0;
@@ -18,12 +18,15 @@ $(document).ready(function() {
     var indexQandA = 0;
     var answered = false;
     var correct;
-        
+
+    // Initial start of game with picture 
     var start = $("#start").html("Start Game");
     start.on("click", startGame);
     $("#start").append("<p /><img src='assets/images/lawPicTrans-01.png' width='200px' height='150px'>");
+    // Final summary page start link
     $("#start-over").on("click", startGame);
-        
+
+    // 8 Question & answer objects
     var triviaQandA = [{
         question:"In which Tennessee city is it illegal to lasso fish?",
         answer:["Nashville","Memphis","Knoxville","Johnson City"],
@@ -66,6 +69,7 @@ $(document).ready(function() {
         image: ("assets/images/sneeze.jpg")
     }];
     
+    // Start of game
     function startGame() {
         $(".summary").hide();
         $("#start").hide();
@@ -79,16 +83,15 @@ $(document).ready(function() {
         loadQandA();
     }
     
+    // loading questions & answers
     function loadQandA() {
         answer = [];
         answered = false;
         timeRemaining = 16;
+        // starting the timer
         timer();
         intervalID = setInterval(timer, 1000);
-        /*if (answered === false) {
-            timer();
-        }*/
-        
+        //  setting up the Q & A's and appending  
         correct = triviaQandA[indexQandA].correct;
         var question = triviaQandA[indexQandA].question;
         $(".question").html(question);
@@ -96,7 +99,8 @@ $(document).ready(function() {
             var answer = triviaQandA[indexQandA].answer[i];
             $(".answers").append("<h4 class = answersAll id =" + i + ">" + answer + "</h4>");
         }
-    
+        
+        // determining if the answer they click on is correct or not
         $("h4").click(function() {
             var id = $(this).attr("id");
             if (id === correct) {
@@ -111,21 +115,24 @@ $(document).ready(function() {
             console.log(correct);
         });
     }
-    
+
+    // timer function
     function timer() {
+        // time is up & they didn't choose an answer
         if (timeRemaining === 0) {
-            answered = true;
-            //clearInterval(intervalID);
+            answered = false;
             $(".question").text("The correct answer is: " + triviaQandA[indexQandA].answer[correct]);
             unAnswered();
+        // time is up & they did choose an answer
         } else if (answered === true) {
-            //clearInterval(intervalID);
+        // else keep decreasing timer
         } else {
             timeRemaining--;
             $(".timeRemaining").text("You have " + timeRemaining + " seconds left");
         }
     }
     
+    // increments the counters for correct, incorrect, and unanswered answers and prints the appropriate text
     function correctAnswer() {
         correctAnswers++;
         $(".timeRemaining").text("You are spot on!!!");
@@ -143,17 +150,21 @@ $(document).ready(function() {
         $(".timeRemaining").text("You didn't choose anything...");
         reset();
     }
-    
+    // ------------------------------------------------------------------------------------------------------- 
+    // game reset   
     function reset() {
         clearInterval(intervalID);
         $(".answersAll").remove();
+        // appends images after each Q & A & increases the indexQandA
         $(".answers").append("<img class=answerImage width='150' height='150' src='" + triviaQandA[indexQandA].image + "'>");
         indexQandA++;
+        // if the indexQandA is less than the length of questions it goes back to the loadQandA function
         if (indexQandA < triviaQandA.length) {
             setTimeout(function () {
                 loadQandA();
                 $(".answerImage").remove();
             }, 4000);
+        // if no more questions it goes to the summary page
         } else {
             setTimeout(function() {
                 $(".summary").show();
@@ -168,11 +179,7 @@ $(document).ready(function() {
                 correctAnswers = 0;
                 incorrectAnswers = 0;
                 unansweredQuestions = 0;
-                indexQandA=0;
-                // setTimeout(function() {
-                //     location.reload();
-                // }, 5000);
-        
+                indexQandA=0;        
                 $("#start-over").html("Start Game");
                 start.on("click", startGame);
             }, 2000);
